3de728cf08eba5955cfa6a7096a05621b183f4f3
Christian Diaz <chris.d.es@outlook.com>
2024-04-25 22:14:08 -0600

    busqueda bindaria recursiva

--- src/mbusqueda/MBusBinRec.java
+++ src/mbusqueda/MBusBinRec.java
@@ -9,5 +9,23 @@ package mbusqueda;
  * @author EQ40
  */
 public class MBusBinRec {
-    
+    public static int MBusBinRec(int[] arr, int izquierdo, int derecho, int number) {
+        /*si el izquierdo es mayor que el derecho esta fuera de la lista*/
+        if (izquierdo > derecho) {
+            return -1;
+        }
+        /*calcular el medio*/
+        int medio = (int) Math.floor(derecho-izquierdo/2);
+        /*si el numero es el que esta a la mitad*/
+        if (number == arr[medio]) {
+            return medio+1;
+        } else if (number < arr[medio]) {
+            /*si el numero a buscar es menor que el que esta a la mitad*/
+            /*se recorre derecho se hace el medio menos 1*/
+            return MBusBinRec(arr, izquierdo, medio - 1, number);
+        } else {
+            /*el izquierdo se mieve uno a la derecha*/
+            return MBusBinRec(arr, medio + 1, derecho, number);
+        }
+    }
 }
